f1ad8415a45c97a418d06a0152bc9fad3f33e331
Joseph Brasher <josephdbrasher@gmail.com>
2025-03-17 22:32:27 -0500

    change to use groups instead of player

--- main.py
+++ main.py
@@ -13,6 +13,9 @@ def main():
     pygame.init()
     game_clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while(True):
@@ -20,8 +23,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, (0, 0, 0))
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        for item in drawable:
+            item.draw(screen)
         pygame.display.flip()
         dt = game_clock.tick(60) / 1000
          
