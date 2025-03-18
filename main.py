0c1b911591c3a8638b6228d81c6e4e5f0cd2d958
Joseph Brasher <josephdbrasher@gmail.com>
2025-03-17 22:13:10 -0500

    draw player and fix pygame import

--- main.py
+++ main.py
@@ -1,24 +1,26 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
-import pygame as pyg
+import pygame
 from constants import * 
+from player import *
 
 def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     
-    pyg.init()
-    game_clock = pyg.time.Clock()
+    pygame.init()
+    game_clock = pygame.time.Clock()
     dt = 0
-    screen = pyg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while(True):
-        for event in pyg.event.get():
-            if event.type == pyg.QUIT:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
                 return
-        pyg.Surface.fill(screen, (0, 0, 0))
-        pyg.display.flip()
+        pygame.Surface.fill(screen, (0, 0, 0))
+        pygame.display.flip()
         dt = game_clock.tick(60) / 1000
          
 if __name__ == "__main__":
