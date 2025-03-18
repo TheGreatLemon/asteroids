1f874a08ef09b6cf054b8c6c8a15725939da40cf
Joseph Brasher <josephdbrasher@gmail.com>
2025-03-17 23:07:16 -0500

    limit rate of fire

--- player.py
+++ player.py
@@ -7,6 +7,7 @@ class Player (circleshape.CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.shot_timer = 0
         
     # in the player class
     def triangle(self):
@@ -30,6 +31,7 @@ class Player (circleshape.CircleShape):
     def shoot(self):
         bullet = Shot(self.position.x, self.position.y)
         bullet.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+        self.shot_timer = PLAYER_SHOOT_COOLDOWN
                
     def update(self, dt):
         keys = pygame.key.get_pressed()
@@ -43,5 +45,8 @@ class Player (circleshape.CircleShape):
         if keys[pygame.K_s]:
             self.move(0 - dt)
         if keys[pygame.K_SPACE]:
-            self.shoot()
+            if self.shot_timer <= 0:
+                self.shoot()
+            else:
+                self.shot_timer -= dt
             
