b9c60c71c7be1ce600d2652e3217ff309a602628
Joseph Brasher <josephdbrasher@gmail.com>
2025-03-17 23:03:52 -0500

    add shooting ability

--- player.py
+++ player.py
@@ -1,6 +1,7 @@
 import circleshape
 from constants import *
 import pygame
+from shot import Shot
 
 class Player (circleshape.CircleShape):
     def __init__(self, x, y):
@@ -25,6 +26,10 @@ class Player (circleshape.CircleShape):
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
+        
+    def shoot(self):
+        bullet = Shot(self.position.x, self.position.y)
+        bullet.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
                
     def update(self, dt):
         keys = pygame.key.get_pressed()
@@ -37,4 +42,6 @@ class Player (circleshape.CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(0 - dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
             
