9e55dfd0fffe3cdf6fb5e2751165833d599250fb
Joseph Brasher <josephdbrasher@gmail.com>
2025-03-17 21:43:47 -0500

    change constants import method

--- main.py
+++ main.py
@@ -2,12 +2,12 @@
 # the open-source pygame library
 # throughout this file
 import pygame
-import constants
+from constants import * 
 
 def main():
     print("Starting Asteroids!")
-    print(f"Screen width: {constants.SCREEN_WIDTH}")
-    print(f"Screen height: {constants.SCREEN_HEIGHT}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
          
 if __name__ == "__main__":
     main()
