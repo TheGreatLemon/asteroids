dcda9e48b720f447371f09abd2bdfbd2d3d93fab
Joseph Brasher <josephdbrasher@gmail.com>
2025-03-17 21:59:45 -0500

    limit tick rate to 60

--- main.py
+++ main.py
@@ -10,13 +10,16 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     
     pyg.init()
+    game_clock = pyg.time.Clock()
+    dt = 0
     screen = pyg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while(True):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+        for event in pyg.event.get():
+            if event.type == pyg.QUIT:
                 return
         pyg.Surface.fill(screen, (0, 0, 0))
         pyg.display.flip()
+        dt = game_clock.tick(60) / 1000
          
 if __name__ == "__main__":
     main()
